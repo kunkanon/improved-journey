f2dead066d4235d2e5be5e10360856c0f6458141
kunkanon <kunkanon@gmail.com>
2017-06-16 17:25:49 -0300

    Create getVariable.js
    A litte function to get the GET variables using JavaScript

---
+++ jquery/getVariable.js
@@ -0,0 +1,17 @@
+/**
+* @Author: Ronyan Alves
+* @Date: 2017-01-05 13:14
+* @Project: JQuery
+*
+**/
+
+function getUrlVars() {
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for (var i = 0; i < hashes.length; i++) {
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	}
