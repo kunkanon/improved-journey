717155ec37b3c80bd9706d1a4846b5079f85684f
kunkanon <kunkanon@gmail.com>
2017-06-23 12:11:27 -0300

    Create xls(x)_creation.php
    A little script to create a xlsx file under Scriptcase on blank application.

---
+++ scriptcase/xls(x)_creation.php
@@ -0,0 +1,20 @@
+##
+# @Author: Ronyan Alves
+# @Date: 2016-05-05 09:44
+# @Project: Scriptcase
+#
+##
+
+require_once('../../../..'.$this->Ini->path_prod.'/third/phpexcel/PHPExcel.php');
+
+$objPHPExcel = new PHPExcel();
+
+
+for($i=0;$i<10;$i++){
+$objPHPExcel->getActiveSheet()->setCellValue('A'.$i, $i)
+							  ->setCellValue('B'.$i, $i)
+							  ->setCellValue('C'.$i, '=SUM(A'.$i.':B'.$i.')');
+	
+}
+$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+$objWriter->save(str_replace('.php', date('s').'.xlsx', __FILE__));
