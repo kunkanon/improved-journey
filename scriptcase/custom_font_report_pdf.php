8d1fe3364045021553ef01c7bc20fe92d5351ba3
kunkanon <kunkanon@gmail.com>
2017-09-01 10:16:59 -0300

    Create custom_font_report_pdf.php

---
+++ scriptcase/custom_font_report_pdf.php
@@ -0,0 +1,10 @@
+<?php
+/** Layout > Code > Body **/
+$pdf_font = new TCPDF_FONTS();
+$fontname = $pdf_font->addTTFfont('/Applications/Scriptcase/v9/wwwroot/f2-tecnocratica-ffp.ttf', 'TrueType', '', 32);
+sc_pdf_add_font($fontname);
+sc_pdf_set_font($fontname);
+[glo_font_style] = $fontname;
+
+/** Layout > Code > Definition **/
+$this->default_font = [glo_font_style];
