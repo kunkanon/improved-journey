6fbcef603a0c70fe779abb092ba3d7d410ca2365
kunkanon <kunkanon@gmail.com>
2018-01-30 15:53:44 -0300

    Create apacheConfigure.sh

---
+++ shellscript/php/apacheConfigure.sh
@@ -0,0 +1,31 @@
+#usage
+#./apacheConfigure.sh /opt/apache24 /opt/apache24/bin/apr /opt/apache24/bin/apr-util /opt/apache24/bin/pcre /root/src/httpd-2.4.29
+#!/bin/bash
+
+APACHEDIR=$1
+APRDIR=$2
+APRUTILDIR=$3
+PCREDIR=$4
+MAINFOLDER=$5
+
+cd $MAINFOLDER
+
+./configure \ 
+--prefix=$APACHEDIR \
+--enable-cgi \
+--enable-so \
+--enable-modules=all \
+--enable-mime-magic \
+--enable-fcgid \
+--enable-ldap=ldap \
+--enable-mods-shared=all \
+--with-apr=$APRDIR \
+--with-apr-util=$APRUTILDIR \
+--with-pcre=$PCREDIR/bin/pcre-config \
+--enable-rewrite \
+--enable-ssl \
+--with-ssl
+
+make
+make install
+make clean
