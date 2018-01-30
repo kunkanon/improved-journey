af4cb77be9a032c375ac9ccf95f3dfdec73eec39
kunkanon <kunkanon@gmail.com>
2018-01-30 15:22:31 -0300

    Update setPHPVersion.sh

--- shellscript/php/setPHPVersion.sh
+++ shellscript/php/setPHPVersion.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
 #usage
 #./setPHPVersion.sh 72 /opt/apache24
+#!/bin/bash
 VERSION=$1
 APACHEDIR=$2
 cp -f $APACHEDIR/conf/extra/fcgi$VERSION.conf $APACHEDIR/conf/extra/mod-fastcgi.conf
