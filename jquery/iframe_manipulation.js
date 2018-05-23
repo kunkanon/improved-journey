45a1dd086c88a32221837daa34c537f34788bc11
kunkanon <kunkanon@gmail.com>
2018-05-22 21:54:21 -0300

    Additional method

--- jquery/iframe_manipulation.js
+++ jquery/iframe_manipulation.js
@@ -5,7 +5,7 @@
 *
 **/
 
-
+/* with count */
 $(document).ready(function(){
 	$("#iframe_treemenu").on("load",function(){
 		$("#iframe_treemenu").contents().find("div#id-div-iframe-0").css("background-color","red");
@@ -13,4 +13,22 @@ $(document).ready(function(){
 		$("#iframe_treemenu").contents().find("div#id-div-iframe-2").css("background-color","yellow");
 		$("#iframe_treemenu").contents().find("div#id-div-iframe-3").css("background-color","orange");
 	})
-})
+});
+
+/* with period */
+$(document).ready(function(){
+	var percent = "";
+	$("#iframe_treemenu_60").on("load",function(){
+		$("#iframe_treemenu_60").contents().find("div#id-div-iframe-4").find("span").each(function(){
+		   if($(this).text().includes("%") == true){
+			  percent = $(this).text().replace("%","");
+			  percent = parseInt(percent);
+		   }
+		});
+		if(percent<50){
+			$("#iframe_treemenu_60").contents().find("div#id-div-iframe-4").css("background-color","rgb(31, 208, 31)");
+		}else{
+			$("#iframe_treemenu_60").contents().find("div#id-div-iframe-4").css("background-color","green");	
+		}
+	})
+});
